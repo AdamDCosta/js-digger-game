6fc26805395c7d5896c9e58b6034335109cf3d71
AdamDCosta <adam.dcosta@googlemail.com>
2021-09-23 14:57:35 +0100

    dynamic grid function added - creates only rows

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
-// Move character
 
 // --> DOM ELEMENTS
 
+const startBtn = document.querySelector(".intro__button")
+
 const userCharacter = document.querySelector(".character")
 
+
+
+// Arrow buttons
+const upArrow = document.querySelector("game__buttons--up")
+const rightArrow = document.querySelector("game__buttons--right")
+const downArrow = document.querySelector("game__buttons--down")
+const leftArrow = document.querySelector("game__buttons--left")
+
+// To move character
+// --> you need arrow button
+// when arrow pressed - character moves in corresponding direction
+
 const gameArea = document.querySelector(".game__area")
+
+const startGame = () => {
+  startBtn.addEventListener("click", (event) => {
+
+  })
+}
+
+const makeGrid = (rows, cols) => {
+  gameArea.style.setProperty('--grid-rows', rows);
+  gameArea.style.setProperty('--grid-cols', cols);
+  for (i = 0; i < (rows * cols); i++) {
+    let cell = document.createElement("div");
+    gameArea.appendChild(cell).className = "game__area--cell";
+  };
+};
+
+makeGrid(4, 4);
